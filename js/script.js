da80345b3f6da91e9890c79bd8e7722be149859b
Donna Richards <DRichards@eftposaustralia.com.au>
2021-01-22 16:55:26 +1100

    upload changes from work laptop

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const jobRoleInput = document.getElementById("title");
+const otherJobRoleInput = document.getElementById("other-job-role");
+
+jobRoleInput.addEventListener("blur", e => {
+    if e.target.value == "other" {
+        otherJobRoleInput.
+    }
+});
