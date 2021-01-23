7c57f65fdd4f0cece6bcca5980afb9d3fb807831
Donna Richards <15822195+DonnaRichards@users.noreply.github.com>
2021-01-23 13:37:04 +1100

    Project requirements 1-4 working

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 const jobRoleInput = document.getElementById("title");
 const otherJobRoleInput = document.getElementById("other-job-role");
 
-jobRoleInput.addEventListener("blur", e => {
-    if e.target.value == "other" {
-        otherJobRoleInput.
+jobRoleInput.addEventListener("change", e => {
+    //  how to get selected option 
+    // https://stackoverflow.com/questions/1085801/get-selected-value-in-dropdown-list-using-javascript
+    if ( jobRoleInput.options[jobRoleInput.selectedIndex].text == "Other" ) {
+        otherJobRoleInput.type = "text";
+    } else {
+        otherJobRoleInput.type = "hidden";
     }
 });
