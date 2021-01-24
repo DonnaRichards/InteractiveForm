e6e4b0b8a1a3a9b7b2f5e274eae2051fcccf6abc
Donna Richards <15822195+DonnaRichards@users.noreply.github.com>
2021-01-24 16:25:24 +1100

    Project requirements #5 (T shirt section) done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 const jobRoleInput = document.getElementById("title");
 const otherJobRoleInput = document.getElementById("other-job-role");
+const shirtDesign = document.getElementById("design");
+const divShirtColors = document.getElementById("shirt-colors");
+const shirtColorOptions = document.querySelectorAll("#color option");
 
-jobRoleInput.addEventListener("change", e => {
+function setVisibilityOtherJobInput() {
     //  how to get selected option 
     // https://stackoverflow.com/questions/1085801/get-selected-value-in-dropdown-list-using-javascript
     if ( jobRoleInput.options[jobRoleInput.selectedIndex].text == "Other" ) {
@@ -9,4 +12,26 @@ jobRoleInput.addEventListener("change", e => {
     } else {
         otherJobRoleInput.type = "hidden";
     }
-});
+};
+
+function setVisShirtColors() {
+    if ( shirtDesign.selectedIndex > 0 ) {
+        divShirtColors.style.display = "initial";
+        shirtColorOptions[0].textContent = "Select a color";
+        for (let i = 1; i < shirtColorOptions.length; i++) {
+            if ( shirtColorOptions[i].dataset.theme == shirtDesign.options[shirtDesign.selectedIndex].value ) {
+                shirtColorOptions[i].hidden = false;
+            } else {
+                shirtColorOptions[i].hidden = true;  
+            }
+        }
+    } else {
+        divShirtColors.style.display = "none";
+        shirtColorOptions[0].textContent = "Select a design theme above";
+    }
+
+}
+
+jobRoleInput.addEventListener("change", setVisibilityOtherJobInput);
+divShirtColors.style.display = "none";
+shirtDesign.addEventListener("change", setVisShirtColors);
